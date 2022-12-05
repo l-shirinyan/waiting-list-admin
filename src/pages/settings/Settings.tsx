815965970e7e21c4eecdfaf9f735800307b10ca9
l-shirinyan <lusinshirinyan@gmail.com>
2022-12-05 15:20:23 +0400

    settings page

--- src/pages/settings/Settings.tsx
+++ src/pages/settings/Settings.tsx
@@ -2,7 +2,7 @@ import { Reservation } from '../../components/reservation/Reservation'
 
 const Settings = () => {
   return (
-    <div className='w-full h-max bg-grey-100 2xl:h-screen'>
+    <div className='w-full h-screen bg-grey-100 sm:h-full 2xl:h-screen'>
       <Reservation />
     </div>
   )
