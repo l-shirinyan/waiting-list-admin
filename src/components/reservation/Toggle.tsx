51545e4bc9554bc0d131f2f82bc64d181648579a
l-shirinyan <lusinshirinyan@gmail.com>
2022-12-05 15:51:18 +0400

    fixed toggle button

--- src/components/reservation/Toggle.tsx
+++ src/components/reservation/Toggle.tsx
@@ -1,9 +1,6 @@
 import { Switch } from '@headlessui/react'
 import { useState } from 'react'
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
 const Toggle = () => {
   const [enabled, setEnabled] = useState(false)
 
@@ -12,17 +9,15 @@ const Toggle = () => {
       <Switch
         checked={enabled}
         onChange={setEnabled}
-        className={classNames(
-          enabled ? 'bg-indigo-600' : 'bg-gray-200',
-          'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2',
-        )}
+        className={`${
+          enabled ? 'bg-indigo-600' : 'bg-gray-200'
+        } relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2`}
       >
         <span
           aria-hidden='true'
-          className={classNames(
-            enabled ? 'translate-x-5' : 'translate-x-0',
-            'pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out',
-          )}
+          className={`${
+            enabled ? 'translate-x-5' : 'translate-x-0'
+          } pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out`}
         />
       </Switch>
     </Switch.Group>
