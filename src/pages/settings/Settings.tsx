d76b3ece0e4756154ff76ce1f6f8b66e86bc656a
l-shirinyan <lusinshirinyan@gmail.com>
2022-12-15 18:01:03 +0400

    some changes

--- src/pages/settings/Settings.tsx
+++ src/pages/settings/Settings.tsx
@@ -1,19 +1,11 @@
 import { Reservation } from '../../components/reservation/Reservation'
-import { useAppSelector } from '../../hooks/redux'
-import { getTerms } from '../../redux/queries'
 
 const Settings = () => {
-  const { identity_id } = useAppSelector((state) => state.isAuth)
-  const { data: termsData, isLoading } = getTerms(`?identity_id=${identity_id}`, 'get', 'termsKey')
-
-  if (isLoading) return <div></div>
-  else {
-    return (
-      <div className='w-full h-max bg-grey-100 pb-[69px]'>
-        <Reservation termsData={termsData} />
-      </div>
-    )
-  }
+  return (
+    <div className='w-full h-max bg-grey-100 pb-[69px]'>
+      <Reservation />
+    </div>
+  )
 }
 
 export { Settings }
